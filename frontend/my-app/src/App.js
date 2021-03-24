5b913049574af4c8f705319f0e7fc19748c433e6
daixingy <daixingyue1996@hotmail.com>
2021-03-23 22:41:16 -0400

    updated

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import sample from './memes/image.jpeg'
+import sample1 from './memes/450_450__1554909083_gymmeme16.jpg'
 
 function App(props) {
-  const subject = "React";
+  const subject = "Have fun browsing memes!!";
   console.log(props)
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+                       
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, {subject}!
+          You've reached the funniest community.
         </p>
+        <p>
+          {subject}
+        </p>
+        
+        <img src={sample} className="memes" alt="meme1" />
+        <img src={sample1} className="memes" alt="meme2" />
+
+       
         
       </header>
     </div>

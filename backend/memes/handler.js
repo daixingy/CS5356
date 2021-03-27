2b10a6981f4c0e727131b2c5ace58c2e5b43c1fb
daixingy <daixingyue1996@hotmail.com>
2021-03-27 00:45:48 -0400

    backend

---
+++ backend/memes/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  if (event.path === '/whoami'&& event.httpMethod === 'GET'){
+    return {
+      statusCode: 200,
+      body: JSON.stringify({username: 'da335'})
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};

dfe020df317b843325298e216a7f3ac6e46e5076
daixingy <daixingyue1996@hotmail.com>
2021-04-20 20:19:52 -0400

    djj

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -6,7 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import SignInScreen from './App';
 
 
-ReactDOM.render(<SignInScreen subject="Clarice"/>, document.getElementById('root'));
+// ReactDOM.render(<SignInScreen subject="Clarice"/>, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <SignInScreen />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-
-reportWebVitals()
+reportWebVitals();
